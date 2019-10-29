d0a4c1e05794cbd93924312cf92ec646fe2faa0d
slinky <QMBFXSL@gmail.com>
2019-10-30 10:38:03 +1300

    thank you kanye

---
+++ Assets/Scripts/trapScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trapScript : MonoBehaviour
+{
+    Animator anim;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            anim.SetTrigger("Spring");
+        }
+    }
+}
